76ad28fc5e75b9bbdc299f2046346732c43e0321
DeGree04 <codeafarg@gmail.com>
2025-01-05 10:59:10 -0300

    Segunda

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Holaaa Loca Flor!!, Como estas?</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
